f7c18e4053b86ce71dd3314ec03ee9d319ad9563
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2021-09-08 14:04:53 +0200

    Wysyłanie wielu komunikatów

--- GrpcConsoleClient/Program.cs
+++ GrpcConsoleClient/Program.cs
@@ -19,18 +19,23 @@ namespace GrpcConsoleClient
             var client = new TrackingService.TrackingServiceClient(channel);
 
             // dotnet add package Bogus
-            AddLocationRequest location = new Faker<AddLocationRequest>()
+            var locations = new Faker<AddLocationRequest>()
                 .RuleFor(p => p.Name, f => f.Vehicle.Model())
                 .RuleFor(p => p.Latitude, f => (float)f.Address.Latitude())
                 .RuleFor(p => p.Longitude, f => (float)f.Address.Longitude())
                 .RuleFor(p => p.Speed, f => f.Random.Int(0, 140))
                 .RuleFor(p => p.Direction, f => f.Random.Float())
-                .Generate();
+                .GenerateForever();
 
-            Console.Write($"Sending {location.Latitude} {location.Longitude} {location.Speed} {location.Direction}");
-            var response = await client.AddLocationAsync(location);
+            foreach (var location in locations)
+            {
+                Console.Write($"Sending {location.Latitude} {location.Longitude} {location.Speed} {location.Direction}");
+                var response = await client.AddLocationAsync(location);
 
-            Console.WriteLine($"{response.IsConfirmed}  Send.");
+                Console.WriteLine($"{response.IsConfirmed}  Send.");
+
+                await Task.Delay(TimeSpan.FromMilliseconds(10));
+            }
 
             Console.WriteLine("Press any key to exit.");
             Console.ReadKey();
