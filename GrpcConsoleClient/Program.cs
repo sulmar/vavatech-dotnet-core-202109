fcc55f139efe8bb78d0caba31c7e0060ad0982c9
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2021-09-08 13:57:45 +0200

    Utworzenie klienta gRPC

--- GrpcConsoleClient/Program.cs
+++ GrpcConsoleClient/Program.cs
@@ -1,12 +1,14 @@
-﻿using Grpc.Net.Client;
+﻿using Bogus;
+using Grpc.Net.Client;
 using System;
+using System.Threading.Tasks;
 using Vavatech.Shop.GrpcServer;
 
 namespace GrpcConsoleClient
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
             Console.WriteLine("Hello gRPC Client!");
 
@@ -16,6 +18,22 @@ namespace GrpcConsoleClient
 
             var client = new TrackingService.TrackingServiceClient(channel);
 
+            // dotnet add package Bogus
+            AddLocationRequest location = new Faker<AddLocationRequest>()
+                .RuleFor(p => p.Name, f => f.Vehicle.Model())
+                .RuleFor(p => p.Latitude, f => (float)f.Address.Latitude())
+                .RuleFor(p => p.Longitude, f => (float)f.Address.Longitude())
+                .RuleFor(p => p.Speed, f => f.Random.Int(0, 140))
+                .RuleFor(p => p.Direction, f => f.Random.Float())
+                .Generate();
+
+            Console.Write($"Sending {location.Latitude} {location.Longitude} {location.Speed} {location.Direction}");
+            var response = await client.AddLocationAsync(location);
+
+            Console.WriteLine($"{response.IsConfirmed}  Send.");
+
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
             
         }
     }
