bb8bd3a28ed24dd6059427d31cfb51c48492cc77
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2021-09-08 08:25:52 +0200

    Refaktoring

--- Vavatech.Shop.ApiConsoleClient/Program.cs
+++ Vavatech.Shop.ApiConsoleClient/Program.cs
@@ -14,15 +14,19 @@ namespace Vavatech.Shop.ApiConsoleClient
 
             int customerId = 10;
 
+            Customer customer = await GetCustomer(customerId);
+
+        }
+
+        private static async Task<Customer> GetCustomer(int customerId)
+        {
             string url = $"https://localhost:5001/api/customers/{customerId}";
 
             var json = await url.GetJsonFromUrlAsync();
 
             var customer = json.FromJson<Customer>();
 
-
+            return customer;
         }
-
-
     }
 }
