cb9f37103d86b14e46916df05255d6a288c68893
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2021-09-08 15:46:14 +0200

    Utworzenie aplikacji webowej MVC

---
+++ Vavatech.Shop.MVCWebApplication/Controllers/CustomersController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Vavatech.Shop.IServices;
+
+namespace Vavatech.Shop.MVCWebApplication.Controllers
+{
+    public class CustomersController : Controller
+    {
+        private readonly ICustomerService customerService;
+
+        public CustomersController(ICustomerService customerService)
+        {
+            this.customerService = customerService;
+        }
+
+        public IActionResult Index()
+        {
+            var customers = customerService.Get();
+
+            return View(customers);
+        }
+
+        public IActionResult Edit(int id)
+        {
+            var customer = customerService.Get(id);
+         
+            return View(customer);
+
+        }
+    }
+}
---
+++ Vavatech.Shop.MVCWebApplication/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Vavatech.Shop.MVCWebApplication.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
