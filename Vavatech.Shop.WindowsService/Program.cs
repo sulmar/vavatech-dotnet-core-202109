f14d0e085ba89b7ff7b80dfecf8826802f48fed8
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2021-09-09 09:58:26 +0200

    Windows Service

---
+++ Vavatech.Shop.WindowsService/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.IO;
+using Topshelf;
+
+namespace Vavatech.Shop.WindowsService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // dotnet add package Topshelf
+
+            Console.WriteLine("Hello World!");
+
+            HostFactory.Run(p =>
+            {
+                p.Service<LoggerService>();
+                p.SetDisplayName("Vavatech Service");
+                p.SetDescription("Opis usługi");
+                p.StartAutomatically();
+            });
+        }
+    }
+
+    public class LoggerService : ServiceControl
+    {
+        public bool Start(HostControl hostControl)
+        {
+            Log("Started!");
+
+            return true;
+        }
+
+        public bool Stop(HostControl hostControl)
+        {
+            Log("Stopped!");
+
+            return true;
+        }
+
+        private void Log(string message)
+        {
+            File.AppendAllText(@"c:\temp\log.txt",  $"{DateTime.Now} {message} " + Environment.NewLine);
+        }
+    }
+
+
+}
